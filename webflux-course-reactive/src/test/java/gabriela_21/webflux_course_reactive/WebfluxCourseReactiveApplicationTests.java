2946109e818d6ee1a82e641daad2d79b7495af5c
gabriela17txm <gabriela17teixeixamendes@uolinc.com>
2024-07-04 23:13:32 -0300

    setup do projeto

---
+++ webflux-course-reactive/src/test/java/gabriela_21/webflux_course_reactive/WebfluxCourseReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package gabriela_21.webflux_course_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
