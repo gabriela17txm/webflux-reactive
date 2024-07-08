d54f247d3356b8a53c930d3ff6df514dd6469acb
gabriela17txm <gabriela17teixeixamendes@uolinc.com>
2024-07-07 23:33:07 -0300

    implementa excecao ao tentar salvar email existente

--- src/main/java/gabriela_21/webflux_course_reactive/controller/exceptions/ControllerExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package gabriela_21.webflux_course_reactive.controller.exceptions;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-public class ControllerExceptionHandler {
-
-}
--- webflux-course-reactive/src/main/java/gabriela_21/webflux_course_reactive/mapper/UserMapper.java
+++ webflux-course-reactive/src/main/java/gabriela_21/webflux_course_reactive/mapper/UserMapper.java
@@ -4,16 +4,9 @@ import gabriela_21.webflux_course_reactive.entity.UserEntity;
 import gabriela_21.webflux_course_reactive.model.request.UserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-
-import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
-import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(
-        componentModel = "spring",
-        nullValuePropertyMappingStrategy = IGNORE,
-        nullValueCheckStrategy = ALWAYS
+        componentModel = "spring"
 )
 public interface UserMapper {
 
