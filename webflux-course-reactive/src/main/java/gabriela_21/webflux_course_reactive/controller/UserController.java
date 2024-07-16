17b702ed115b84a89353392525c23ea31e9ffe4f
gabriela17txm <gabriela17teixeixamendes@uolinc.com>
2024-07-15 22:51:34 -0300

    adiciona exception para erro de validacao dos atributos

--- webflux-course-reactive/src/main/java/gabriela_21/webflux_course_reactive/controller/UserController.java
+++ webflux-course-reactive/src/main/java/gabriela_21/webflux_course_reactive/controller/UserController.java
@@ -2,6 +2,7 @@ package gabriela_21.webflux_course_reactive.controller;
 
 import gabriela_21.webflux_course_reactive.model.request.UserRequest;
 import gabriela_21.webflux_course_reactive.model.response.UserResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -9,7 +10,7 @@ import reactor.core.publisher.Mono;
 
 public interface UserController {
     @PostMapping
-    ResponseEntity<Mono<Void>> save(@RequestBody UserRequest request);
+    ResponseEntity<Mono<Void>> save(@Valid @RequestBody UserRequest request);
 
     @GetMapping("/{id}")
     ResponseEntity<Mono<UserResponse>> find(@PathVariable String id);
